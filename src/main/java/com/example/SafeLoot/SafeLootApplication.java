612fe4e699603ea128990505d9be99c02aa0bf58
AntonStankov <antonstankov007@gmail.com>
2023-03-09 10:41:12 +0200

    initialized spring project

---
+++ src/main/java/com/example/SafeLoot/SafeLootApplication.java
@@ -0,0 +1,13 @@
+package com.example.SafeLoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeLootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeLootApplication.class, args);
+	}
+
+}

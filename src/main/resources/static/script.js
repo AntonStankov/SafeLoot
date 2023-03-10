0dfb26da6953962471a2d2023119222f54ebd4a1
BorisStoyanv <101361204+BorisStoyanv@users.noreply.github.com>
2023-03-10 17:14:35 +0200

    Small fetching and changing

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -95,6 +95,48 @@ function authenticate(){
 }
 })
 
-    
-
 }
+
+function generateAndDisplayPassword() {
+    var passwordLength = document.getElementById("passwordLength").value;
+    if (!passwordLength) {
+      alert("Please enter password length");
+      return;
+    }
+    
+    var applet = document.getElementById("myApplet");
+    var password = applet.generatePassword(passwordLength);
+    document.getElementById("passwordOutput").innerHTML = password;
+  }
+
+  function addpassword(){
+    let _passname = document.getElementById("passname").value
+    let _password = document.getElementById("password").value
+    let _url = document.getElementById("url").value
+
+    var x = document.getElementById("snackbar");
+    fetch('http://localhost:8085/user/savePassword  ', {
+        method: 'post',
+        body: JSON.stringify({
+            passName: _passname,
+            password: _password,
+            url : _url,
+
+            isActive: true //default
+        }),
+        headers: new Headers({'content-type': 'application/json',
+                            'authorization': 'Bearer ',
+                        }),
+        }
+    )
+    .then( async (response) => {
+
+    // get json response here
+    let data = await response.json();
+    console.log(response.status)
+    if(response.status === 200){
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
+})
+  }
